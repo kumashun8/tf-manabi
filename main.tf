a4c9676dad30e2efebcacfac505f0f4dd22ab00f
kumashun8 <tiwo3wa4@gmail.com>
2020-06-12 14:53:59 +0900

    Use locals

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+# ローカル変数
+locals {
+  default_ami = "ami-0c3fd0f5d33134a76"
+}
+
 variable "kumashun_instance_type" {
   default = "t3.micro"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c3fd0f5d33134a76"
+  ami           = local.default_ami
   instance_type = var.kumashun_instance_type
 
   tags = {
